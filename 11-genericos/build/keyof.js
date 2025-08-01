8e4d90116863941934534674df237c33c08ceeea
danielvargassanchezAxity <79526143+danielvargassanchezAxity@users.noreply.github.com>
2025-08-01 15:21:48 -0600

    last changes

--- 11-genericos/build/keyof.js
+++ 11-genericos/build/keyof.js
@@ -3,16 +3,17 @@ function filterByPropertie(array, key, filterValue) {
     return array.filter((object) => object[key] === filterValue);
 }
 class Book {
-    constructor(name, author, date, pages) {
+    constructor(name, author, date, pages, isActive) {
         this.name = name;
         this.author = author;
         this.date = date;
         this.pages = pages;
+        this.isActive = isActive;
     }
 }
 const date = new Date('01/01/2000');
-const firstBook = new Book("El principito", "NOSE", new Date('06/06/1992'), 572);
-const secondBook = new Book("Harry Potter", "otro nose", date, 200);
-const thirdBook = new Book("El principito 3", "NOSE", new Date('06/06/1992'), 572);
-const results = filterByPropertie([firstBook, secondBook, thirdBook], "date", date);
+const firstBook = new Book("El principito", "NOSE", new Date('06/06/1992'), 572, false);
+const secondBook = new Book("Harry Potter", "otro nose", date, 200, true);
+const thirdBook = new Book("El principito 3", "NOSE", new Date('06/06/1992'), 572, true);
+const results = filterByPropertie([firstBook, secondBook, thirdBook], "isActive", false);
 console.log(results);
--- 11-genericos/src/keyof.ts
+++ 11-genericos/src/keyof.ts
@@ -7,18 +7,20 @@ class Book {
     author: string;
     date: Date;
     pages: number;
+    isActive: boolean;
 
-    constructor(name: string, author: string, date: Date, pages: number) {
+    constructor(name: string, author: string, date: Date, pages: number, isActive: boolean) {
         this.name = name;
         this.author = author;
         this.date = date;
         this.pages = pages;
+        this.isActive = isActive;
     }
 }
 const date = new Date('01/01/2000');
-const firstBook = new Book("El principito", "NOSE", new Date('06/06/1992'), 572);
-const secondBook = new Book("Harry Potter", "otro nose", date, 200);
-const thirdBook = new Book("El principito 3", "NOSE", new Date('06/06/1992'), 572);
+const firstBook = new Book("El principito", "NOSE", new Date('06/06/1992'), 572, false);
+const secondBook = new Book("Harry Potter", "otro nose", date, 200, true);
+const thirdBook = new Book("El principito 3", "NOSE", new Date('06/06/1992'), 572, true);
 
-const results = filterByPropertie([firstBook, secondBook, thirdBook], "date", date);
+const results = filterByPropertie([firstBook, secondBook, thirdBook], "isActive", false);
 console.log(results);
